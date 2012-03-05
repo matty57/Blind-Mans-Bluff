6ed3a1fde0d39da38a1d9330f615f751d9b83fd8
matty57 <matthew.studer@nwktc.edu>
2012-03-05 10:39:30 -0700

    Initial Commit

---
+++ blind mans bluff3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  blind mans bluff3
+//
+//  Created by Matthew Studer on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ blind mans bluff3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  blind mans bluff3
+//
+//  Created by Matthew Studer on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
