b9a190d861018232a21f44fa1c38a39aacaa3092
matty57 <matthew.studer@nwktc.edu>
2012-03-05 13:08:53 -0700

    added a card class

---
+++ blind mans bluff3/Card.h
@@ -0,0 +1,13 @@
+//
+//  Card.h
+//  blind mans bluff3
+//
+//  Created by Matthew Studer on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Card : NSObject
+
+@end
---
+++ blind mans bluff3/Card.m
@@ -0,0 +1,13 @@
+//
+//  Card.m
+//  blind mans bluff3
+//
+//  Created by Matthew Studer on 3/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Card.h"
+
+@implementation Card
+
+@end
