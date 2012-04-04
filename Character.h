a295c9284331d4dcba9606bdac5e7ad996338540
matty57 <matthew.studer@nwktc.edu>
2012-04-04 10:38:38 -0600

    Added Character Class and some methods

---
+++ Character.h
@@ -0,0 +1,24 @@
+//
+//  Character.h
+//  blind mans bluff3
+//
+//  Created by Matthew Studer on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Character : NSObject
+//Code Created by Matthew
+@property (nonatomic, strong) NSString * name;
+@property int chips;
+-(int)requestBetWithMinimum:(int) minimumBet;
+
+//Do you have enough chips to ante, if not your out.
+-(BOOL)anteUpWithAmount:(int) anteAmount;
+
+//Add chips, play winning sound.
+-(void)awardChips:(int) chipsAmount;
+    
+//End of Code Created by Matthew
+@end
---
+++ Character.m
@@ -0,0 +1,13 @@
+//
+//  Character.m
+//  blind mans bluff3
+//
+//  Created by Matthew Studer on 4/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Character.h"
+
+@implementation Character
+
+@end
